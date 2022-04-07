17fecae145eb6cdc8d8ac0d8b32b0576abd777a5
Gregory Galicio <gregory.galicios@ciplima.org.pe>
2022-04-07 18:32:56 -0500

    Form creado

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, {useEffect, useState } from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 //import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 import {fireStore} from './firebase/firebase';
+import Form from "./Form";
 
 
 export default function App() {
   const[data,setData]=useState([]);
   useEffect(()=>{
-    fireStore.collection('tweets').get()
+    fireStore
+      .collection('tweets')
+      .get()
       .then((snapshot) => {
-        const docs=[]
-        snapshot.forEach(doc =>{
-          console.warn(doc.data())
-          docs.push(doc.data())
+        const docs= snapshot.map(doc =>{
+          return{
+            tweet: doc.data().tweet,
+            author:doc.data().author,
+            id: doc.id
+          }
         })
           setData(docs)
         //console.warn(snapshot)
@@ -23,18 +28,18 @@ export default function App() {
     <BrowserRouter>
     <div className="App">
       <h1>Hola mundo</h1>
+      <Form data={data} setData={setData}/>
       {
-        
         data.map(item=>(
-          <div>
+          <div key={item}>
             <p>{item.tweet}</p>
-            <p>{item.username}</p>
+            <p>{item.author}</p>
             <p>{item.task}</p>
             <p>{item.done}</p>
           </div>
         ))
       }
-    </div>
+    </div> 
     </BrowserRouter>
     
   );
---
+++ src/Form.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import useForm from "./useForm";
+import { fireStore } from "./firebase/firebase";
+
+const Form= ({
+    data=[],
+    setData
+})=>{
+    const [value, handleInput, setValue]=useForm({
+        tweet:"",
+        author:"",
+    })
+    const {tweet, author}= value;
+
+    function handleSubmit (e) {
+        e.preventDefault()
+        //añadiendo tweets
+        const addTweet =fireStore.collection("tweets").add(value)  /*Se coloca value en vez de colocar tweet:{tweet} y author:{author}*/
+        const getDocument= addTweet.then(doc =>(doc.get()))
+        getDocument.then(doc =>{
+            const newTweet={
+                tweet: doc.data().tweet,
+                author:doc.data().author,
+                id: doc.id
+            }
+            setData([newTweet,...data])  
+        });
+
+        setValue({
+            tweet:"",
+            author:"" 
+        });
+        
+    }
+    return (
+        <form className="tweet-form">
+            <textarea
+                name='tweet'
+                value={tweet}
+                onChange={handleInput}
+            >
+            </textarea>
+            <input
+                name='author'
+                placeholder='Author'
+                value={author}
+                onChange={handleInput}
+            />
+            <button
+                className="btn-tweet"
+                onClick={handleSubmit}
+            >
+                Submit
+            </button>
+        </form>
+    )
+
+}    
+export default Form
