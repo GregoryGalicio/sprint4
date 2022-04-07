4d06693715cd1e2df19ed8ba872aeefde75c2bb5
Gregory Galicio <gregory.galicios@ciplima.org.pe>
2022-04-06 21:42:28 -0500

    creando api keys firebase

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>Hola mundo</h1>
     </div>
   );
 }
---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+/*Dependencias */
+
+import firebaseApp from 'firebase/app';
+import 'firebase/firestore';
+/**Config */
+
+import config from './config';
+
+
+firebaseApp.initializeApp(config);
+
+export const fireStore = firebaseApp.firestore();
+
+export default firebaseApp;
