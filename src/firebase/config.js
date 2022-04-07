ebfceb3cefe1798eef54f04cbf42bb2d650ed837
Gregory Galicio <gregory.galicios@ciplima.org.pe>
2022-04-07 15:18:03 -0500

    coneccion de firebase con archivo oculto .env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,4 @@
-const config = {
+/*const config = {
   apiKey: "AIzaSyDBXLuxsH_8IVW5k0UqPIJDL9x8ZjU0mQQ",
   authDomain: "devs-united-245e8.firebaseapp.com",
   projectId: "devs-united-245e8",
@@ -6,15 +6,14 @@ const config = {
   messagingSenderId: "685250525395",
   appId: "1:685250525395:web:0f7bd9c5695973049dfc26"
 };
-export default config
+export default config*/
 
-/*const config = {
+const config = {
     apiKey: process.env.REACT_APP_FB_API_KEY,
     authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FB_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FB_MSN_SENDER_ID,
     appId: process.env.REACT_APP_FB_APPID,
-  };
-  
-  export default config;*/
+  };  
+  export default config;
